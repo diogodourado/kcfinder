eb1abe88dce237ab85c993d9040ba753615b329b
diogodourado <diogo@dourado.net>
2014-10-06 14:30:29 -0300

    Brazilian portuguese translation update
    fix encoding, update and add texts

--- lang/pt-br.php
+++ lang/pt-br.php
@@ -1,63 +1,60 @@
 <?php
-
-/** Portugues Brasil localization file for KCFinder
-  * author: Alexandre Benegas Ferreira
-  * country: Brasil
-  * company website: www.hostcataratas.com.br
-  * e-mail: contato@hostcataratas.com.br
+/** Brazilian Portuguese localization file for KCFinder
+  * author: Alexandre Benegas Ferreira <alexbenegas@hotmail.com>
+  * update: Diogo Dourado <diogo@dourado.net>
   */
 
 $lang = array(
 
     '_lang' => "Brazilian Portuguese",
-    '_native' => "Português do Brasil",
+    '_native' => "Português Brasileiro",
     '_locale' => "pt_BR.UTF-8",  // UNIX localization code
     '_charset' => "utf-8",       // Browser charset
 
     // Date time formats. See http://www.php.net/manual/en/function.strftime.php
-    '_dateTimeFull' => "%A, %e %B, %Y %H:%M",
-    '_dateTimeMid' => "%a %e %b %Y %H:%M",
-    '_dateTimeSmall' => "%d.%m.%Y %H:%M",
+    '_dateTimeFull' => "%A, %e/%B/%Y %H:%M",
+    '_dateTimeMid' => "%a, %e/%b/%y %H:%M",
+    '_dateTimeSmall' => "%d/%m/%Y %H:%M",
 
-    "You don't have permissions to upload files." => "Você não tem permissões para fazer upload de arquivos.",
-    "You don't have permissions to browse server." => "Você não tem permissões para procurar no servidor.",
-    "Cannot move uploaded file to target folder." => "Não é poss&iacutevel mover o arquivo enviado para a pasta de destino.",
+    "You don't have permissions to upload files." => "Você não possui permissões para fazer upload de arquivos.",
+    "You don't have permissions to browse server." => "Você não possui permissões para procurar arquivos no servidor.",
+    "Cannot move uploaded file to target folder." => "Não é possível mover o arquivo enviado para a pasta de destino.",
     "Unknown error." => "Erro desconhecido.",
-    "The uploaded file exceeds {size} bytes." => "O arquivo enviado excede {size} bytes.",
-    "The uploaded file was only partially uploaded." => "O arquivo enviado foi apenas parcialmente carregado.",
+    "The uploaded file exceeds {size} bytes." => "O arquivo enviado é maior do que {size} bytes.",
+    "The uploaded file was only partially uploaded." => "O arquivo foi apenas enviado parcialmente.",
     "No file was uploaded." => "Nenhum arquivo foi transferido.",
     "Missing a temporary folder." => "Faltando uma pasta temporária.",
     "Failed to write file." => "Falha ao gravar arquivo.",
     "Denied file extension." => "Extensão de arquivo não permitida.",
-    "Unknown image format/encoding." => "Formato de imagem desconhecido/codificação.",
+    "Unknown image format/encoding." => "Formato de imagem codificado ou desconhecido.",
     "The image is too big and/or cannot be resized." => "A imagem é muito grande e/ou não pode ser redimensionada.",
-    "Cannot create {dir} folder." => "Não é poss&iacutevel criar pasta em '{dir}'.",
-    "Cannot rename the folder." => "Não é poss&iacutevel renomear a pasta.",
-    "Cannot write to upload folder." => "Não é poss&iacutevel salvar na pasta.",
-    "Cannot read .htaccess" => "Não é poss&iacutevel ler '.htaccess'.",
-    "Incorrect .htaccess file. Cannot rewrite it!" => "Arquivo '.htaccess' incorreto. Não é poss&iacutevel alterar.",
-    "Cannot read upload folder." => "Não é poss&iacutevel ler a pasta de upload.",
-    "Cannot access or create thumbnails folder." => "Não é poss&iacutevel acessar ou criar pasta de miniaturas.",
-    "Cannot access or write to upload folder." => "Não é poss&iacutevel acessar ou salvar para a pasta.",
+    "Cannot create {dir} folder." => "Não é possível criar pasta em '{dir}'.",
+    "Cannot rename the folder." => "Não é possível renomear a pasta.",
+    "Cannot write to upload folder." => "Não é possível salvar na pasta.",
+    "Cannot read .htaccess" => "Não é possível ler '.htaccess'.",
+    "Incorrect .htaccess file. Cannot rewrite it!" => "Arquivo '.htaccess' incorreto. Não é possível alterar.",
+    "Cannot read upload folder." => "Não é possível ler a pasta de upload.",
+    "Cannot access or create thumbnails folder." => "Não é possível acessar ou criar pasta de miniaturas.",
+    "Cannot access or write to upload folder." => "Não é possível acessar ou salvar para a pasta.",
     "Please enter new folder name." => "Por favor, digite o nome da nova pasta.",
-    "Unallowable characters in folder name." => "Caracteres no nome da pasta não Autorizado.",
+    "Unallowable characters in folder name." => "Caracteres no nome da pasta não são autorizados.",
     "Folder name shouldn't begins with '.'" => "Nome da pasta não deve começar com '.'.",
     "Please enter new file name." => "Por favor, digite o novo nome de arquivo.",
-    "Unallowable characters in file name." => "Caracteres no nome do arquivo não Autorizado.",
+    "Unallowable characters in file name." => "Caracteres no nome do arquivo não são utorizados.",
     "File name shouldn't begins with '.'" => "O nome da pasta não deve começar por '.'.",
     "Are you sure you want to delete this file?" => "Tem a certeza de que deseja excluir este arquivo?",
-    "Are you sure you want to delete this folder and all its content?" => "Tem a certeza de que deseja excluir esta pasta e todo o seu conte�do?",
+    "Are you sure you want to delete this folder and all its content?" => "Tem a certeza de que deseja excluir esta pasta e todo o seu conteúdo?",
     "Non-existing directory type." => "Tipo de diretório não existente.",
     "Undefined MIME types." => "Tipos MIME indefinidos.",
-    "Fileinfo PECL extension is missing." => "Está faltando Informações do arquivo extensão PECL.",
+    "Fileinfo PECL extension is missing." => "Está faltando informações do arquivo extensão PECL.",
     "Opening fileinfo database failed." => "Abrir banco de dados de fileinfo falhou.",
     "You can't upload such files." => "Você não pode enviar esses arquivos.",
     "The file '{file}' does not exist." => "O arquivo '{file}' não existe.",
-    "Cannot read '{file}'." => "Não é poss&iacutevel ler '{file}'.",
-    "Cannot copy '{file}'." => "Não é poss&iacutevel copiar '{file}'.",
-    "Cannot move '{file}'." => "Não é poss&iacutevel mover '{file}'.",
-    "Cannot delete '{file}'." => "Não é poss&iacutevel deletar '{file}'.",
-    "Cannot delete the folder." => "Não é poss&iacutevel excluir a pasta.",
+    "Cannot read '{file}'." => "Não é possível ler '{file}'.",
+    "Cannot copy '{file}'." => "Não é possível copiar '{file}'.",
+    "Cannot move '{file}'." => "Não é possível mover '{file}'.",
+    "Cannot delete '{file}'." => "Não é possível deletar '{file}'.",
+    "Cannot delete the folder." => "Não é possível excluir a pasta.",
     "Click to remove from the Clipboard" => "Clique para remover da área de transferência",
     "This file is already added to the Clipboard." => "Este arquivo já foi adicionado à área de transferência.",
     "The files in the Clipboard are not readable." => "Os arquivos da área de transferência não podem ser lidos.",
@@ -65,11 +62,11 @@ $lang = array(
     "The files in the Clipboard are not movable." => "Os arquivos da área de transferência não podem ser removidos.",
     "{count} files in the Clipboard are not movable. Do you want to move the rest?" => "{count} arquivos da área de transferência não podem ser movidos. Você deseja mover o resto?",
     "The files in the Clipboard are not removable." => "Os arquivos da área de transferência não podem ser removidos.",
-    "{count} files in the Clipboard are not removable. Do you want to delete the rest?" => "{count} arquivos da área de transferência não são remov&iacuteveis. Você deseja excluir o restante?",
-    "The selected files are not removable." => "Os arquivos selecionados não são remov&iacuteveis.",
-    "{count} selected files are not removable. Do you want to delete the rest?" => "{count} arquivos selecionados não são remov&iacuteveis. Você deseja excluir o restante?",
+    "{count} files in the Clipboard are not removable. Do you want to delete the rest?" => "{count} arquivos da área de transferência não são removíveis. Você deseja excluir o restante?",
+    "The selected files are not removable." => "Os arquivos selecionados não são removíveis.",
+    "{count} selected files are not removable. Do you want to delete the rest?" => "{count} arquivos selecionados não são removíveis. Você deseja excluir o restante?",
     "Are you sure you want to delete all selected files?" => "Tem a certeza de que deseja excluir todos os arquivos selecionados?",
-    "Failed to delete {count} files/folders." => "Não conseguiu excluir {count} arquivos/pastas.",
+    "Failed to delete {count} files/folders." => "Não foi possível excluir {count} arquivos/pastas.",
     "A file or folder with that name already exists." => "Já existe um arquivo ou pasta com esse nome.",
     "Copy files here" => "Copiar arquivos aqui",
     "Move files here" => "Mover arquivos aqui",
@@ -79,9 +76,9 @@ $lang = array(
     "Copy {count} files" => "Copiar {count} arquivos",
     "Move {count} files" => "Mover {count} arquivos",
     "Add to Clipboard" => "Adicionar à área de transferência",
-    "Inexistant or inaccessible folder." => "Pasta inacess&iacutevel ou inexistente.",
+    "Inexistant or inaccessible folder." => "Pasta inacessível ou inexistente.",
     "New folder name:" => "Nome da nova pasta:",
-    "New file name:" => "Novo nome do arquivo:",
+    "New file name:" => "Nome do novo arquivo:",
     "Upload" => "Enviar arquivo",
     "Refresh" => "Atualizar",
     "Settings" => "Configurações",
@@ -126,5 +123,12 @@ $lang = array(
     "No" => "Não",
     "You cannot rename the extension of files!" => "Você não pode renomear a extensão de arquivos!",
     "Uploading file {number} of {count}... {progress}" => "Enviando arquivo {number} de {count}... {progress}",
-    "Failed to upload {filename}!" => "Falha no envio do arquivo {filename}!",
+    "Failed to upload {filename}!" => "Falha ao enviar arquivo {filename}!",
+    "Warning" => "Atenção",
+    "Close" => "Fechar",
+    "Previous" => "Anterior",
+    "Next" => "Próximo",
+    "Confirmation" => "Confirmação"
 );
+
+?>
